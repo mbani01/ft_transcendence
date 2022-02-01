15f346d2b703b6fa51d329d3a8cc4927419aa583
Houssam-eddine Moumanis <hmoumani@e2r11p10.1337.ma>
2022-02-01 16:30:20 +0100

    setting docker-compose infra

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const PORT : number = 8000;
-  const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
-}
-bootstrap();
