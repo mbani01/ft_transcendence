702ec283ce89f508911649eeb458667dd00ca28f
mbani <mouad-mb@outlook.com>
2022-02-02 16:32:09 +0100

    pulling from main

--- front/src/app/chat/shared/chat-room.model.ts
+++
@@ -1,8 +0,0 @@
-import {User} from "../../shared/user";
-import {Message} from "./message.model";
-
-export class ChatRoom {
-  private users: User[] = [];
-  private messages: Message[] = [];
-  private unread: number = 0;
-}
