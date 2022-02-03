14e37fa4e9ba14ae0b3d65c2abc1641dada3def3
mbani <mouad-mb@outlook.com>
2022-02-03 16:36:16 +0100

    setup TypeORM migrations

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GameModule } from './modules/game/game.module';
+import { SocketModule } from './modules/socket/socket.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({isGlobal: true}), GameModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), GameModule, SocketModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
