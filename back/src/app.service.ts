7d22168cbe04af0b31cf0ad376399c2251c6ff10
mbani <mouad-mb@outlook.com>
2022-02-03 16:36:16 +0100

    setup TypeORM migrations

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { socketGateway } from './modules/socket/socket.gateway';
 
 @Injectable()
 export class AppService {
+  constructor(private socketGateway: socketGateway){}
   getHello(): string {
     return 'Hello World!';
   }
