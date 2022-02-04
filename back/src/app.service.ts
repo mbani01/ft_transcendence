644186d5b851ba1b78cd2c236b5a9f88d7f44bec
mbani <mouad-mb@outlook.com>
2022-02-04 11:15:26 +0100

    add socket adapter (ability to use middleware)

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -3,7 +3,6 @@ import { socketGateway } from './modules/socket/socket.gateway';
 
 @Injectable()
 export class AppService {
-  constructor(private socketGateway: socketGateway){}
   getHello(): string {
     return 'Hello World!';
   }
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { WsAdapter } from './adapters/socket.adapter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new WsAdapter(80));
   app.setGlobalPrefix('api');
   await app.listen(PORT);
   console.log("App Listening on Port : " + PORT);
