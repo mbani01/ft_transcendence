614d1ad1b20a6fb46e0bccc756661b66cd53166d
mbani <mouad-mb@outlook.com>
2022-02-04 14:58:50 +0100

    update socket port for heroku

--- src/modules/socket/socket.gateway.ts
+++ src/modules/socket/socket.gateway.ts
@@ -6,14 +6,14 @@
 /*   By: mbani <mbani@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 09:51:02 by mbani             #+#    #+#             */
-/*   Updated: 2022/02/04 10:58:33 by mbani            ###   ########.fr       */
+/*   Updated: 2022/02/04 14:58:27 by mbani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { CustomSocket } from "src/adapters/socket.adapter";
 
-@WebSocketGateway(80)
+@WebSocketGateway(8080)
 export class socketGateway {
   @SubscribeMessage('message')
   handleMessage(client: CustomSocket, message: any): void {
