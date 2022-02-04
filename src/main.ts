958a2bb9184c11e9ccfaab9e849d5456a1b3bac5
mbani <mouad-mb@outlook.com>
2022-02-04 15:07:27 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(80));
   app.setGlobalPrefix('api');
+  app.enableCors()
   await app.listen(PORT);
   console.log("App Listening on Port : " + PORT);
 }
