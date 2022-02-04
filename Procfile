0839a10b93eb00af764d4a29927a8b030f692be7
mbani <mouad-mb@outlook.com>
2022-02-04 17:38:25 +0100

    setup config for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-npm build && npm start
+web: npm run start:prod
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   app.useWebSocketAdapter(new WsAdapter(80));
   app.setGlobalPrefix('api');
-  app.enableCors()
   await app.listen(PORT);
   console.log("App Listening on Port : " + PORT);
 }
