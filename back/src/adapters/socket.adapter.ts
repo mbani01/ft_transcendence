1383a28b92bf82af86dc7647e9bec8d61d04b6f7
mbani <mouad-mb@outlook.com>
2022-02-04 11:19:07 +0100

    socket adapter implemented

---
+++ back/src/adapters/socket.adapter.ts
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   socket.adapter.ts                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbani <mbani@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/04 09:50:56 by mbani             #+#    #+#             */
+/*   Updated: 2022/02/04 11:14:10 by mbani            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { Socket } from 'socket.io';
+
+export interface CustomSocket extends Socket { 
+	user: any;
+}
+  
+export class WsAdapter extends IoAdapter {
+	createIOServer(port: number, options?: any) {
+		const server = super.createIOServer(port, options);
+		server.use((socket :Socket, next: any)=>{
+			//Verify jwt token Here !
+			// if (Verified)
+			// 	next();
+			// else
+			// 	next (new Error("Unauthorized !"));
+			next();
+		});
+		return server;
+	}
+}
