44822e34bad000156571b8d0007b0671f7355f5e
mbani <mouad-mb@outlook.com>
2022-02-04 15:13:59 +0100

    enable socket cors

--- src/modules/socket/socket.gateway.ts
+++ src/modules/socket/socket.gateway.ts
@@ -6,14 +6,14 @@
 /*   By: mbani <mbani@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 09:51:02 by mbani             #+#    #+#             */
-/*   Updated: 2022/02/04 14:58:27 by mbani            ###   ########.fr       */
+/*   Updated: 2022/02/04 15:13:46 by mbani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { CustomSocket } from "src/adapters/socket.adapter";
 
-@WebSocketGateway(8080)
+@WebSocketGateway(8080, { cors: true })
 export class socketGateway {
   @SubscribeMessage('message')
   handleMessage(client: CustomSocket, message: any): void {
