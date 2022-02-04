e9966de0dc249beda9faf74c814c770112e444b9
M0-san <moboustta6@gmail.com>
2022-02-04 18:35:03 +0100

    add the needed configuration to typeORM -> used the env values in docker compose file

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -7,7 +7,21 @@ import { GameModule } from './modules/game/game.module';
 import { SocketModule } from './modules/socket/socket.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), GameModule, SocketModule, TypeOrmModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GameModule,
+    SocketModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres', // Since we are using PostgreSQL.
+      host: 'localhost', // We are devoloping locally.
+      port: 5432, // What we set in our docker-compose file.
+      username: 'postgres', // ""
+      password: 'example', // "pretty straightforward haha"
+      database: 'postgres', // db name.
+      autoLoadEntities: true, // help load entities automatically.
+      synchronize: true, // insures our entities are sync with the database every time we run our app.
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
