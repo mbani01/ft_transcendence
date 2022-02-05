95b59660c7f8fbe27e4879b26bb1d17b3928d00b
M0-san <moboustta6@gmail.com>
2022-02-05 15:15:37 +0100

    refactor src code

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,6 +4,7 @@ import { UsersController } from './users.controller';
 
 @Module({
   exports: [UsersService], // export User's BL to be available for other modules.
-  providers: [UsersService], controllers: [UsersController],
+  providers: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
