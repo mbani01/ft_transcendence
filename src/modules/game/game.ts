ac1fe66d4cf1f8d2ceb427b381f4e67ce4224f1e
mbani <mouad-mb@outlook.com>
2022-02-14 11:38:50 +0100

    sync score

--- src/modules/game/game.ts
+++ src/modules/game/game.ts
@@ -6,7 +6,7 @@
 /*   By: mbani <mbani@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 12:00:29 by mbani             #+#    #+#             */
-/*   Updated: 2022/02/10 17:45:10 by mbani            ###   ########.fr       */
+/*   Updated: 2022/02/14 11:38:34 by mbani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,24 @@ type ball = {
 	x: number;
 	y: number;
 }
+
+type score = {
+	player1: number;
+	player2: number;
+}
 export class Game{
 	isPublic: boolean;
 	Players: Array<any>;
 	GameId: string;
 	ballPos: Array<number> = [200, -200];
 	ball: ball;
+	score: score;
 	
 	constructor(isPublic: boolean, Players: Array<any>)
 	{
 		this.isPublic = isPublic;
 		this.Players = Players;
+		this.score = {player1: 0, player2: 0};
 		this.GameId = String(Date.now());
 		this.ball = {x: ((Math.floor(Math.random() * (100 - 2) + 2) % 2) === 0) ? this.ballPos[0] : this.ballPos[1],
 			 y:((Math.floor(Math.random() * (100 - 2) + 2) % 2) === 0) ? this.ballPos[0] : this.ballPos[1]};
@@ -55,5 +62,10 @@ export class Game{
 	getInfos()
 	{
 		return {GameId: this.GameId, ball: this.ball};
+	}
+	updateScore(score: score)
+	{
+		this.score = score;
+		console.log("current Score : " + this.score);
 	}
 }
