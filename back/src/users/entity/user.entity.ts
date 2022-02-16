1d172aaadebc5281714e760a3dd933a6517d132e
M0-san <moboustta6@gmail.com>
2022-02-16 15:53:02 +0100

    add users srcs to main branch

---
+++ back/src/users/entity/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('Users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  avatar?: string;
+
+  @Column({ default: 0 })
+  gamesCount?: number;
+
+  @Column({ default: 0 })
+  gamesWon?: number;
+
+  @Column({ default: false })
+  is2FAEnabled?: boolean;
+
+  @Column({ nullable: true })
+  twoFASecret?: string;
+}
