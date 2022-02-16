beba9637d34f10a83bdbfb44e8d5a3bca506bc6f
M0-san <moboustta6@gmail.com>
2022-02-16 16:51:39 +0100

    verify jwt token when socket adapter connects

--- back/src/adapters/socket.adapter.ts
+++ back/src/adapters/socket.adapter.ts
@@ -13,28 +13,36 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { Socket } from 'socket.io';
 import { activeUsers } from '../modules/socket/activeUsers';
+// import * as jwt from 'jsonwebtoken';
+// import { JwtConstants } from 'src/auth/constants';
 
-export interface CustomSocket extends Socket { 
+export interface CustomSocket extends Socket {
 	user: any;
 }
-  
+
 const Clients = new activeUsers();
 export class WsAdapter extends IoAdapter {
+
 	createIOServer(port: number, options?: any) {
-		const server = super.createIOServer(port, { cors: true});
-		server.use((socket :CustomSocket, next: any)=>{
-			const user = {"username": "mbani"};
+		const server = super.createIOServer(port, { cors: true });
+		server.use((socket: CustomSocket, next: any) => {
+			console.log("here");
+			const user = { "username": "mbani" };
 			socket.user = socket.handshake.headers.token || user;
+			// const token = socket.user;
+			// const decoded = jwt.verify(token, JwtConstants.jwtSecret);
+			// console.log(decoded);
 			//Verify jwt token Here !
 			// if (Verified)
 			// 	next();
 			// else
 			// 	next (new Error("Unauthorized !"));
-			Clients.add('1234mbani', 'this is socketId')
+			// Clients.add('1234mbani', 'this is socketId')
+			console.log("try to connect");
 			next();
 		});
 		return server;
 	}
 }
 
-export {Clients}
+export { Clients }
