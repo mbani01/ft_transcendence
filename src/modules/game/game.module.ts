cb827bbd2060ddbe03ea2235ead72703ceed3c48
mbani <mouad-mb@outlook.com>
2022-02-16 14:39:08 +0100

    merging with mbani (adding socket/game modules)

--- src/modules/game/game.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GameController } from './game.controller';
-import { gameSocketGateway } from './gameSocket.gateway';
-
-@Module({
-  providers: [gameSocketGateway],
-  controllers: [GameController]
-})
-export class GameModule {}
