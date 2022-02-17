c53fedecda46296767798478e27f887452c332b3
mbani <mouad-mb@outlook.com>
2022-02-17 18:17:22 +0100

    enable static serve (merging front with back)

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -25,7 +25,7 @@ export class AuthController {
   @Get('/oauth_page')
   getOauthPage() {
     return {
-      page: 'https://api.intra.42.fr/oauth/authorize?client_id=802dd62932bab69d1e0168a42ed0567a6b83267c134fb2f62d583c14b2a4ce97&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Flogin&response_type=code ',
+      page: 'https://api.intra.42.fr/oauth/authorize?client_id=802dd62932bab69d1e0168a42ed0567a6b83267c134fb2f62d583c14b2a4ce97&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code ',
     };
   }
 
