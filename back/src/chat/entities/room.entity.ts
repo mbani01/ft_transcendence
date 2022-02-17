05f56e22be1f49f1694c32021dba774373084d61
Houssam-eddine Moumanis <hmoumani@student.1337.ma>
2022-02-17 03:51:08 +0100

    adding many-to-many relation to chat tables

--- back/src/chat/entities/room.entity.ts
+++ back/src/chat/entities/room.entity.ts
@@ -1,5 +1,8 @@
-import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/users/entity/user.entity";
+import { UsersController } from "src/users/users.controller";
+import { Column, Entity, JoinTable, ManyToMany, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 import { ChannelType } from "../common/chat.types";
+import { MembersEntity } from "./members.entity";
 
 @Entity('room') // crate a table named rooms.
 export class RoomEntity {
@@ -20,4 +23,11 @@ export class RoomEntity {
 
     @Column({ default: '' })
     password: string;
+
+    @OneToMany(() => MembersEntity, member => member.room)
+    public memberShip!: MembersEntity[];
+    
+    @OneToMany(() => MembersEntity, member => member.room)
+    public messages!: MembersEntity[];
+
 }
