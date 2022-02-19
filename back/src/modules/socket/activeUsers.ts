fa4a6071e9fa717c52b6140832d529e9099a38a0
mbani <mouad-mb@outlook.com>
2022-02-19 15:15:14 +0100

    update ActiveUsers class

--- back/src/modules/socket/activeUsers.ts
+++ back/src/modules/socket/activeUsers.ts
@@ -6,7 +6,7 @@
 /*   By: mbani <mbani@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 14:12:49 by mbani             #+#    #+#             */
-/*   Updated: 2022/02/15 14:39:15 by mbani            ###   ########.fr       */
+/*   Updated: 2022/02/19 14:48:17 by mbani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,34 @@ export class activeUsers {
 	private users = new Map();
 
 	add(userId: string, socketId: string) {
-		this.users.set(socketId, userId);
+		this.users.set(userId, socketId);
 		// this.users.push({userId: UserId, socketId: SocketId});
 	}
 
-	removeBySocketId(socketId: string) {
-		this.users.delete(socketId);
+	remove(userId: string) {
+		this.users.delete(userId);
 		// this.users = this.users.filter(element => element.userId !== UserId);
 	}
 
-	removeByUserId(userId: String) {
-		this.users.forEach((value: string, key: string) => {
-			if (value === userId)
-				this.users.delete(key);
-		});
-		// this.users = this.users.filter(element => element.socketId !== socketId);
-	}
+	// removeByUserId(userId: String) {
+	// 	this.users.forEach((value: string, key: string) => {
+	// 		if (value === userId)
+	// 			this.users.delete(key);
+	// 	});
+	// 	// this.users = this.users.filter(element => element.socketId !== socketId);
+	// }
 
-	getUserId(socketId: string) {
-		return this.users.get(socketId);
+	getSocketId(userId: string) {
+		return this.users.get(userId);
 	}
-
-	isActiveUser(socketId: string) {
-		return (this.users.has(socketId));
+	
+	get()
+	{
+		return this.users;
+	}
+	
+	isActiveUser(userId: string) {
+		return (this.users.has(userId));
 		// const user = this.users.find(element=> element.userId === UserId);
 	}
 }
