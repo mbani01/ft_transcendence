6e5a3f2314c445fa41097ef15ef85dec11f70bc3
ojoubout <oussamajoubouti@gmail.com>
2022-02-19 02:20:07 +0100

    check authentication loading & 2fa fix

--- front/src/app/login/oauth.service.ts
+++ front/src/app/login/oauth.service.ts
@@ -11,7 +11,7 @@ import {BehaviorSubject, catchError, throwError} from "rxjs";
 })
 export class OAuthService {
 
-  private access_token: boolean = false;
+  private _authorized?: boolean;
   private _is2FA = false;
   // @ts-ignore
   public user$: BehaviorSubject<User> = new BehaviorSubject<User>(null);
@@ -21,15 +21,18 @@ export class OAuthService {
     // this.access_token =;
     this.http.get(`${environment.apiBaseUrl}/auth/isAuthorized`).subscribe({
       next: value => {
-        this.access_token = true;
+        this._authorized = true;
         console.log("Authorized");
-        this.router.navigate(['']);
+        // this.router.navigate([window.location.pathname]);
 
       },
       error: err => {
-        this.access_token = false;
+        this._authorized = false;
         console.log("Not Authorized");
-        this.router.navigate(['login']);
+        console.log(router.url);
+        if (!router.url.startsWith('/login')) {
+          this.router.navigate(['login']);
+        }
       }
     })
     setTimeout(() => {
@@ -46,7 +49,7 @@ export class OAuthService {
     if (_2fa != undefined) {
       params = params.set("twoFactorAuth", _2fa);
     }
-    console.log(params);
+    console.log('GENERATE');
     let obs = this.http.post(environment.apiBaseUrl + '/auth/access_token', null, {
       params: params
     });
@@ -55,27 +58,15 @@ export class OAuthService {
         console.log('TOKEN');
         // console.log(this.cookieService'access_token'));
         this.router.navigate(['']);
-        this.access_token = true;
+        this._authorized = true;
         // this.cookieService.set('access_token', this.access_token, undefined, '/');
         // this.user$.next(token.user);
-      },
-      error: (error => {
-        if (error.error['2FA']) {
-          this._is2FA = true;
-        }
-        console.log(error)
-      }),
-      // complete: () => this.router.navigate([''])
-    });
+      }});
     return obs;
   }
 
   isAuthenticated() {
-    return this.access_token;
-  }
-
-  get token() {
-    return this.access_token;
+    return this._authorized;
   }
 
   get user() {
@@ -95,7 +86,7 @@ export class OAuthService {
 
   logout() {
     this.cookieService.delete('access_token', '/');
-    this.access_token = false;
+    this._authorized = false;
   }
 
 
--- front/src/app/login/two-factor-auth/two-factor-auth.component.ts
+++ front/src/app/login/two-factor-auth/two-factor-auth.component.ts
@@ -1,8 +1,8 @@
-import {Component} from "@angular/core";
+import {Component, EventEmitter, Output} from "@angular/core";
 import {NgForm} from "@angular/forms";
 import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {OAuthService} from "../oauth.service";
 
 @Component({
@@ -15,10 +15,22 @@ export class TwoFactorAuthComponent {
   private readonly code: string;
   // public is2FA = false;
   public loading = false;
+  public is2FA = false;
+  @Output() fail = new EventEmitter();
 
-  constructor(private http: HttpClient, private route: ActivatedRoute, public oauthService: OAuthService) {
+  constructor(private http: HttpClient, private route: ActivatedRoute, public oauthService: OAuthService, private router: Router) {
     this.code = route.snapshot.queryParams['code'];
-    this.oauthService.generateAccessToken(this.code);
+    console.log("2FA");
+    this.oauthService.generateAccessToken(this.code).subscribe({
+      error: err => {
+        if (err.error['2FA']) {
+          this.is2FA = true;
+        } else {
+          this.fail.emit();
+          router.navigate(['login']);
+        }
+      }
+    });
   }
 
   verify(twoFactorAuth: NgForm) {
@@ -26,6 +38,9 @@ export class TwoFactorAuthComponent {
     console.log(twoFactorAuth);
     this.loading = true;
     this.oauthService.generateAccessToken(this.code, twoFactorAuthCode).subscribe({
+      next: value => {
+        this.oauthService.enable2FA();
+      },
       error: (err: HttpErrorResponse) => {
         twoFactorAuth.controls['twoFactorAuth'].setErrors(err.error);
         this.loading = false;
