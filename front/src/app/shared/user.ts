0bc02129b9a1e2305fc9f3098799bc368427c763
ojoubout <oussamajoubouti@gmail.com>
2022-02-26 22:09:44 +0100

    finishing upload avatar

--- front/src/app/shared/user.ts
+++ front/src/app/shared/user.ts
@@ -1,12 +1,5 @@
-export class User {
-
-  constructor(public uid: number, public name: string) {
-
-  }
-
-  get img() {
-    // console.log('hello ?');
-    return `/assets/avatars/${this.uid}`;
-  }
-
+export interface User {
+  uid: number;
+  name: string;
+  img: string;
 }
